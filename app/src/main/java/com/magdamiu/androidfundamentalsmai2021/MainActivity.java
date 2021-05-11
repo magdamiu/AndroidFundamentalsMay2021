03d9a27f686eebc5227aa136c85f16f8275c1fc8
magdamiu <badita.magda@gmail.com>
2021-05-11 21:41:51 +0300

    initial commit - course 1

---
+++ app/src/main/java/com/magdamiu/androidfundamentalsmai2021/MainActivity.java
@@ -0,0 +1,19 @@
+package com.magdamiu.androidfundamentalsmai2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.e("tag-error", "my first error log");
+        Log.v("tag-verbose", "my first verbose log");
+        Log.w("tag-warning", "my first warning log");
+    }
+}
