bc71f8513bc24f917f3b0718de1b7965549f34e3
Stefan923 <stefanpopescu923@gmail.com>
2021-05-24 14:58:07 +0300

    Added work for Code Challenge 1 (week 2)

--- Curs-1/DemoApp/app/src/main/java/com/example/demoapp/MainActivity.java
+++ Curs-1/DemoApp/app/src/main/java/com/example/demoapp/MainActivity.java
@@ -3,25 +3,16 @@ package com.example.demoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "DemoApp";
-
-    private static final String NOT_A_NUMBER = "not a number";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        try {
-            Integer.parseInt(NOT_A_NUMBER);
-        } catch (NumberFormatException e) {
-            Log.e(TAG, e.getMessage());
-        }
-
     }
 
 }
