e9845a5f699255262e2382c93aad40e6ba6a487f
Stefan-Theodor Ionica <ionicastefan20@gmail.com>
2021-05-25 00:24:15 +0300

    Stefan-Theodor Ionica - Curs 2 - Code Challenge 1

--- app/src/main/java/com/magdamiu/androidfundamentalsmai2021/MainActivity.java
+++ app/src/main/java/com/magdamiu/androidfundamentalsmai2021/MainActivity.java
@@ -12,49 +12,9 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editTextName;
-    private Button buttonDisplayGreetings;
-    private TextView textViewGreetings;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*setContentView(R.layout.activity_main);*/
-        setContentView(R.layout.sample);
-
-        initViews();
-        displayLogs();
-    }
-
-    private void initViews() {
-        editTextName = findViewById(R.id.editTextName);
-        buttonDisplayGreetings = findViewById(R.id.buttonDisplayGreetings);
-        textViewGreetings = findViewById(R.id.textViewGreetings);
-    }
-
-    private void displayLogs() {
-        Log.e("tag-error", "my first error log");
-        Log.v("tag-verbose", "my first verbose log");
-        Log.w("tag-warning", "my first warning log");
-    }
-
-    private void clickOnDisplayGreetingsButton() {
-        buttonDisplayGreetings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-    }
-
-    public void displayGreetingsOnClick(View view) {
-        textViewGreetings.setText("");
-        String inputName = editTextName.getText().toString();
-        if (inputName.length() > 0) {
-            textViewGreetings.setText(inputName);
-        } else {
-            editTextName.setError(getString(R.string.error_insert_name));
-            Toast.makeText(MainActivity.this, getString(R.string.error_insert_name), Toast.LENGTH_LONG).show();
-        }
+        setContentView(R.layout.activity_main);
     }
 }
