649f698640fde10d3af08037cd23acba204a5bb5
Gloria <glorya.popa@gmail.com>
2021-05-25 12:44:27 +0300

    Code challenge - c2

--- app/src/main/java/com/magdamiu/androidfundamentalsmai2021/MainActivity.java
+++ app/src/main/java/com/magdamiu/androidfundamentalsmai2021/MainActivity.java
@@ -19,8 +19,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*setContentView(R.layout.activity_main);*/
-        setContentView(R.layout.sample);
+        /* setContentView(R.layout.activity_main);
+        setContentView(R.layout.sample); */
+        setContentView(R.layout.code_challenge_c2);
 
         initViews();
         displayLogs();
