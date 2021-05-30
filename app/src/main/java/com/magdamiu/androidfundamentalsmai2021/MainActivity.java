a3197328df4f860072eb4706afd48068eb44b212
Gloria <glorya.popa@gmail.com>
2021-05-31 00:32:06 +0300

    Course 3 - Code Challenges

--- app/src/main/java/com/magdamiu/androidfundamentalsmai2021/MainActivity.java
+++ app/src/main/java/com/magdamiu/androidfundamentalsmai2021/MainActivity.java
@@ -3,9 +3,10 @@ package com.magdamiu.androidfundamentalsmai2021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String ANDROID_URL = "https://developer.android.com/";
     private EditText editTextName;
+    private EditText editTextEmail;
+    private EditText editTextPhone;
     private Button buttonDisplayGreetings;
     private TextView textViewGreetings;
     private WebView webView;
@@ -36,16 +39,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.sample);
         setContentView(R.layout.code_challenge_c2);
         setContentView(R.layout.scrollview_sample);
-        setContentView(R.layout.webview_sample); */
+        setContentView(R.layout.webview_sample);
         setContentView(R.layout.spinner_sample);
+        setContentView(R.layout.code_challenge2_c3); */
+        setContentView(R.layout.code_challenge1_c3);
 
-        setAndroidVersions();
+        /* setAndroidVersions();
         initSpinnerAdapter();
         setSpinnerAdapter();
-        handlingSpinnerListener();
+        handlingSpinnerListener(); */
 
         // loadUrl();
-        // initViews();
+        initViews();
         displayLogs();
     }
 
@@ -100,6 +105,8 @@ public class MainActivity extends AppCompatActivity {
         editTextName = findViewById(R.id.editTextName);
         buttonDisplayGreetings = findViewById(R.id.buttonDisplayGreetings);
         textViewGreetings = findViewById(R.id.textViewGreetings);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPhone = findViewById(R.id.editTextPhone);
     }
 
     private void displayLogs() {
@@ -125,6 +132,30 @@ public class MainActivity extends AppCompatActivity {
         } else {
             editTextName.setError(getString(R.string.error_insert_name));
             Toast.makeText(MainActivity.this, getString(R.string.error_insert_name), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // Course 3: Code Challenge 1
+    public static boolean isValidEmail(CharSequence target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
+    }
+
+    public static boolean isValidPhone(CharSequence phone) {
+        return (!TextUtils.isEmpty(phone) && Patterns.PHONE.matcher(phone).matches());
+    }
+
+    public void displaySubmitOnClick(View view) {
+        String inputName = editTextEmail.getText().toString();
+        if (!isValidEmail(inputName)) {
+            editTextEmail.setError(getString(R.string.error_insert_email));
+            Toast.makeText(MainActivity.this, getString(R.string.error_insert_email),
+                    Toast.LENGTH_LONG).show();
+        }
+        inputName = editTextPhone.getText().toString();
+        if (!isValidPhone(inputName)) {
+            editTextPhone.setError(getString(R.string.error_insert_phone));
+            Toast.makeText(MainActivity.this, getString(R.string.error_insert_phone),
+                    Toast.LENGTH_LONG).show();
         }
     }
 }
