f72917e052847b34b1662b6577c39ce043086b14
Madalin <clipicimadalin10@gmail.com>
2021-06-13 14:02:00 +0300

    Comment the unnecessary code.

--- app/src/main/java/com/magdamiu/androidfundamentalsmai2021/MainActivity.java
+++ app/src/main/java/com/magdamiu/androidfundamentalsmai2021/MainActivity.java
@@ -18,31 +18,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String ANDROID_URL = "https://developer.android.com/";
-
-    private EditText editTextName;
-    private Button buttonDisplayGreetings;
-    private TextView textViewGreetings;
-
-    private WebView webView;
-
-    private Spinner spinnerAndroidVersions;
-    private List<String> androidVersions;
-    private ArrayAdapter<String> spinnerAdapter;
+//    private static final String ANDROID_URL = "https://developer.android.com/";
+//
+//    private EditText editTextName;
+//    private Button buttonDisplayGreetings;
+//    private TextView textViewGreetings;
+//
+//    private WebView webView;
+//
+//    private Spinner spinnerAndroidVersions;
+//    private List<String> androidVersions;
+//    private ArrayAdapter<String> spinnerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*setContentView(R.layout.activity_main);*/
+        setContentView(R.layout.activity_main);
         /*setContentView(R.layout.sample);*/
         // setContentView(R.layout.scrollview_sample);
         // setContentView(R.layout.webview_sample);
-        setContentView(R.layout.spinner_sample);
+//        setContentView(R.layout.spinner_sample);
 
-        setAndroidVersions();
-        initialiseSpinnerAdapter();
-        setSpinnerAdapter();
-        handlingSpinnerListener();
+//        setAndroidVersions();
+//        initialiseSpinnerAdapter();
+//        setSpinnerAdapter();
+//        handlingSpinnerListener();
 
         //loadUrl();
 
@@ -50,58 +50,58 @@ public class MainActivity extends AppCompatActivity {
         displayLogs();
     }
 
-    // step 1: data source for our spinner
-    private void setAndroidVersions() {
-        androidVersions = new ArrayList<>();
-        androidVersions.add("cupcake");
-        androidVersions.add("eclair");
-        androidVersions.add("pie");
-        androidVersions.add("donut");
-        androidVersions.add("kitkat");
-    }
-
-    // step 2: initialise adapter for our spinner
-    // ArrayAdapter is a default adapter
-    private void initialiseSpinnerAdapter() {
-        spinnerAdapter = new ArrayAdapter<>(MainActivity.this,
-                android.R.layout.simple_spinner_item,
-                androidVersions);
-    }
-
-    // step 3: set the adapter to our spinner
-    private void setSpinnerAdapter() {
-        spinnerAndroidVersions = findViewById(R.id.spinnerAndroidVersions);
-        spinnerAndroidVersions.setAdapter(spinnerAdapter);
-    }
-
-    private void handlingSpinnerListener() {
-        spinnerAndroidVersions.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                String selectedAndroidVersion = androidVersions.get(i);
-                Toast.makeText(MainActivity.this, selectedAndroidVersion, Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-    }
-
-    // webview load url
-    private void loadUrl() {
-        webView = findViewById(R.id.webViewSample);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl(ANDROID_URL);
-    }
-
-    // init views for implementing a first simple form
-    private void initViews() {
-        editTextName = findViewById(R.id.editTextName);
-        buttonDisplayGreetings = findViewById(R.id.buttonDisplayGreetings);
-        textViewGreetings = findViewById(R.id.textViewGreetings);
-    }
+//    // step 1: data source for our spinner
+//    private void setAndroidVersions() {
+//        androidVersions = new ArrayList<>();
+//        androidVersions.add("cupcake");
+//        androidVersions.add("eclair");
+//        androidVersions.add("pie");
+//        androidVersions.add("donut");
+//        androidVersions.add("kitkat");
+//    }
+//
+//    // step 2: initialise adapter for our spinner
+//    // ArrayAdapter is a default adapter
+//    private void initialiseSpinnerAdapter() {
+//        spinnerAdapter = new ArrayAdapter<>(MainActivity.this,
+//                android.R.layout.simple_spinner_item,
+//                androidVersions);
+//    }
+//
+//    // step 3: set the adapter to our spinner
+//    private void setSpinnerAdapter() {
+//        spinnerAndroidVersions = findViewById(R.id.spinnerAndroidVersions);
+//        spinnerAndroidVersions.setAdapter(spinnerAdapter);
+//    }
+//
+//    private void handlingSpinnerListener() {
+//        spinnerAndroidVersions.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//                String selectedAndroidVersion = androidVersions.get(i);
+//                Toast.makeText(MainActivity.this, selectedAndroidVersion, Toast.LENGTH_LONG).show();
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> adapterView) {
+//
+//            }
+//        });
+//    }
+//
+//    // webview load url
+//    private void loadUrl() {
+//        webView = findViewById(R.id.webViewSample);
+//        webView.getSettings().setJavaScriptEnabled(true);
+//        webView.loadUrl(ANDROID_URL);
+//    }
+//
+//    // init views for implementing a first simple form
+//    private void initViews() {
+//        editTextName = findViewById(R.id.editTextName);
+//        buttonDisplayGreetings = findViewById(R.id.buttonDisplayGreetings);
+//        textViewGreetings = findViewById(R.id.textViewGreetings);
+//    }
 
     private void displayLogs() {
         Log.e("tag-error", "my first error log");
@@ -109,23 +109,23 @@ public class MainActivity extends AppCompatActivity {
         Log.w("tag-warning", "my first warning log");
     }
 
-    private void clickOnDisplayGreetingsButton() {
-        buttonDisplayGreetings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+//    private void clickOnDisplayGreetingsButton() {
+//        buttonDisplayGreetings.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+//    }
 
-            }
-        });
-    }
-
-    public void displayGreetingsOnClick(View view) {
-        textViewGreetings.setText("");
-        String inputName = editTextName.getText().toString();
-        if (inputName.length() > 0) {
-            textViewGreetings.setText(inputName);
-        } else {
-            editTextName.setError(getString(R.string.error_insert_name));
-            Toast.makeText(MainActivity.this, getString(R.string.error_insert_name), Toast.LENGTH_LONG).show();
-        }
-    }
+//    public void displayGreetingsOnClick(View view) {
+//        textViewGreetings.setText("");
+//        String inputName = editTextName.getText().toString();
+//        if (inputName.length() > 0) {
+//            textViewGreetings.setText(inputName);
+//        } else {
+//            editTextName.setError(getString(R.string.error_insert_name));
+//            Toast.makeText(MainActivity.this, getString(R.string.error_insert_name), Toast.LENGTH_LONG).show();
+//        }
+//    }
 }
