588b6fa53d8a69bb1efb502c228eda54c7930c56
Madalin <clipicimadalin10@gmail.com>
2021-06-14 18:27:36 +0300

    optimize the imports.

--- app/src/main/java/com/magdamiu/androidfundamentalsmai2021/MainActivity.java
+++ app/src/main/java/com/magdamiu/androidfundamentalsmai2021/MainActivity.java
@@ -1,21 +1,8 @@
 package com.magdamiu.androidfundamentalsmai2021;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.webkit.WebView;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
