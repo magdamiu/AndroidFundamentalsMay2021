09a79282c9f442d4a6cd95743f4d0e44798bc2ce
magdamiu <badita.magda@gmail.com>
2021-07-06 21:26:14 +0300

    start activity for result & debug

--- app/src/main/java/com/magdamiu/androidfundamentalsmai2021/activities/FirstActivity.java
+++ app/src/main/java/com/magdamiu/androidfundamentalsmai2021/activities/FirstActivity.java
@@ -1,5 +1,6 @@
 package com.magdamiu.androidfundamentalsmai2021.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.magdamiu.androidfundamentalsmai2021.R;
@@ -17,13 +19,19 @@ import static com.magdamiu.androidfundamentalsmai2021.R.string.use_device_with_s
 
 public class FirstActivity extends AppCompatActivity {
     protected static final String MESSAGE = "message";
+    protected static final int REQUEST_CODE_FIRST_ACTIVITY = 255;
     private static final String FIRST_TAG = "FirstActivity";
 
-     @Override
+
+    private EditText editTextMessage;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
         Log.e(FIRST_TAG, "onCreate method was called");
+
+        editTextMessage = findViewById(R.id.editTextMessage);
     }
 
     @Override
@@ -84,4 +92,24 @@ public class FirstActivity extends AppCompatActivity {
         openURLInBrowser.setData(Uri.parse("http://www.twitter.com"));
         startActivity(openURLInBrowser);
     }
+
+    public void sendMessageOnClick(View view) {
+        String message = editTextMessage.getText().toString();
+        if (message.length() > 0) {
+            Intent startSecondActivity = new Intent(FirstActivity.this, SecondActivity.class);
+            startSecondActivity.putExtra(MESSAGE, message);
+            startActivityForResult(startSecondActivity, REQUEST_CODE_FIRST_ACTIVITY);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_FIRST_ACTIVITY && resultCode == RESULT_OK) {
+            String messageReceivedAsResult = data.getStringExtra(MESSAGE);
+            editTextMessage.setText(messageReceivedAsResult);
+        }
+    }
+
 }
