cf1c87c18aa72000fa1f7cb0e8909d64a311dfe7
Madalin <clipicimadalin10@gmail.com>
2021-07-07 18:27:00 +0300

    add and implement the code for solving the Course4CodeChallenge3.

---
+++ app/src/main/java/com/magdamiu/androidfundamentalsmai2021/Course4CodeChallenge3.java
@@ -0,0 +1,14 @@
+package com.magdamiu.androidfundamentalsmai2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Course4CodeChallenge3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course4_code_challenge3);
+    }
+}
