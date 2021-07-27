93d25809b33633b0ff89823fb24ad0e338a59443
magdamiu <badita.magda@gmail.com>
2021-07-27 21:16:02 +0300

    material design

--- app/src/main/java/com/magdamiu/androidfundamentalsmai2021/styles/CardViewActivity.java
+++ app/src/main/java/com/magdamiu/androidfundamentalsmai2021/styles/CardViewActivity.java
@@ -1,11 +1,17 @@
 package com.magdamiu.androidfundamentalsmai2021.styles;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.magdamiu.androidfundamentalsmai2021.R;
 
 public class CardViewActivity extends AppCompatActivity {
@@ -24,5 +30,22 @@ public class CardViewActivity extends AppCompatActivity {
         animationDrawable.setEnterFadeDuration(2500);
         animationDrawable.setExitFadeDuration(5000);
         animationDrawable.start();
+    }
+
+    public void fabOnClick(View view) {
+        Snackbar snackbar = Snackbar
+                .make(view, R.string.error_occured, Snackbar.LENGTH_LONG)
+                .setAction(R.string.retry, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(CardViewActivity.this, getString(R.string.retry_message), Toast.LENGTH_LONG).show();
+                    }
+                });
+        snackbar.setActionTextColor(Color.RED);
+        View snackbarView = snackbar.getView();
+        snackbarView.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_200));
+        TextView textView = snackbarView.findViewById(com.google.android.material.R.id.snackbar_text);
+        textView.setTextColor(Color.GREEN);
+        snackbar.show();
     }
 }
